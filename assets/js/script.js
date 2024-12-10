d2b0bee2b5b83d15e16b19c42f9eef6e82f7afeb
Clément Humez <zheum60@gmail.com>
2024-12-10 11:27:09 +0100

    Starter du payement

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,24 @@
 // fin enzo 
 
 // debut clem 
+const stripe = require('stripe')('sk_test_51QUPnHFhiydSL0HhfqTv2s5DosXmrbaV6Vg1zpL2Wws5djqzmKzpQrWH2GuDK1SVFg1dETLHCSjc95GJA5XmDjDD00ahdizCvC');
 
+stripe.products.create({
+    name: 'Starter Subscription',
+    description: '$12/Month subscription',
+}).then(product => {
+    stripe.prices.create({
+        unit_amount: 1200,
+        currency: 'usd',
+        recurring: {
+            interval: 'month',
+        },
+        product: product.id,
+    }).then(price => {
+        console.log('Success! Here is your starter subscription product id: ' + product.id);
+        console.log('Success! Here is your starter subscription price id: ' + price.id);
+    });
+});
 // fin clem 
 
 // debut jules 
